067f28c2fbe729ccb2b193691a9dc8dae80d201a
Xin Yao <55143094+xyao0709@users.noreply.github.com>
2020-02-12 23:19:57 -0500

    Make simon available

--- HW0/js/main.js
+++ HW0/js/main.js
@@ -1,62 +1,219 @@
-"use strict";
+//copied from Phaser 2 examples simon.
 
-function make_main_game_state( game )
-{
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-    }
-    
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
-    }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
-    }
-    
-    return { "preload": preload, "create": create, "update": update };
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+    game.load.spritesheet('item', 'assets/buttons/number-buttons.png', 160, 160);
 }
 
+var simon;
+var N = 1;
+var userCount = 0;
+var currentCount = 0;
+var sequenceCount = 16;
+var sequenceList = [];
+var simonSez = false;
+var timeCheck;
+var litSquare;
+var winner;
+var loser;
+var intro;
 
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
-    
-    game.state.add( "main", make_main_game_state( game ) );
-    
-    game.state.start( "main" );
-};
+function create() {
+
+    simon = game.add.group();
+    var item;
+
+    for (var i = 0; i < 3; i++)
+    {
+        item = simon.create(150 + 168 * i, 150, 'item', i);
+        // Enable input.
+        item.inputEnabled = true;
+        item.input.start(0, true);
+        item.events.onInputDown.add(select);
+        item.events.onInputUp.add(release);
+        item.events.onInputOut.add(moveOff);
+        simon.getAt(i).alpha = 0;
+    }
+
+    for (var i = 0; i < 3; i++)
+    {
+        item = simon.create(150 + 168 * i, 318, 'item', i + 3);
+        // Enable input.
+        item.inputEnabled = true;
+        item.input.start(0, true);
+        item.events.onInputDown.add(select);
+        item.events.onInputUp.add(release);
+        item.events.onInputOut.add(moveOff);
+        simon.getAt(i + 3).alpha = 0;
+    }
+
+    introTween();
+    setUp();
+    setTimeout(function(){simonSequence(); intro = false;}, 6000);
+
+}
+
+function restart() {
+
+    N = 1;
+    userCount = 0;
+    currentCount = 0;
+    sequenceList = [];
+    winner = false;
+    loser = false;
+    introTween();
+    setUp();
+    setTimeout(function(){simonSequence(); intro=false;}, 6000);
+
+}
+
+function introTween() {
+
+    intro = true;
+
+    for (var i = 0; i < 6; i++)
+    {
+        var flashing = game.add.tween(simon.getAt(i)).to( { alpha: 1 }, 500, Phaser.Easing.Linear.None, true, 0, 4, true);
+        var final = game.add.tween(simon.getAt(i)).to( { alpha: .25 }, 500, Phaser.Easing.Linear.None, true);
+
+        flashing.chain(final);
+        flashing.start();
+    }
+
+}
+
+function update() {
+
+    if (simonSez)
+    {
+        if (game.time.now - timeCheck >700-N*40)
+        {
+            simon.getAt(litSquare).alpha = .25;
+            game.paused = true;
+
+            setTimeout(function()
+            {
+                if ( currentCount< N)
+                {
+                    game.paused = false;
+                    simonSequence();
+                }
+                else
+                {
+                    simonSez = false;
+                    game.paused = false;
+                }
+            }, 400 - N * 20);
+        }
+    }
+}
+
+function playerSequence(selected) {
+
+    correctSquare = sequenceList[userCount];
+    userCount++;
+    thisSquare = simon.getIndex(selected);
+
+    if (thisSquare == correctSquare)
+    {
+        if (userCount == N)
+        {
+            if (N == sequenceCount)
+            {
+                winner = true;
+                setTimeout(function(){restart();}, 3000);
+            }
+            else
+            {
+                userCount = 0;
+                currentCount = 0;
+                N++;
+                simonSez = true;
+            }
+        }
+    }
+    else
+    {
+        loser = true;
+        setTimeout(function(){restart();}, 3000);
+    }
+
+}
+
+function simonSequence () {
+
+    simonSez = true;
+    litSquare = sequenceList[currentCount];
+    simon.getAt(litSquare).alpha = 1;
+    timeCheck = game.time.now;
+    currentCount++;
+
+}
+
+function setUp() {
+
+    for (var i = 0; i < sequenceCount; i++)
+    {
+        thisSquare = game.rnd.integerInRange(0,5);
+        sequenceList.push(thisSquare);
+    }
+
+}
+
+function select(item, pointer) {
+
+    if (!simonSez && !intro && !loser && !winner)
+    {
+        item.alpha = 1;
+    }
+
+}
+
+function release(item, pointer) {
+
+    if (!simonSez && !intro && !loser && !winner)
+    {
+        item.alpha = .25;
+        playerSequence(item);
+    }
+}
+
+function moveOff(item, pointer) {
+
+    if (!simonSez && !intro && !loser && !winner)
+    {
+        item.alpha = .25;
+    }
+
+}
+
+function render() {
+
+    if (!intro)
+    {
+        if (simonSez)
+        {
+            game.debug.text('Simon Sez', 360, 96, 'rgb(255,0,0)');
+        }
+        else
+        {
+            game.debug.text('Your Turn', 360, 96, 'rgb(0,255,0)');
+        }
+    }
+    else
+    {
+        game.debug.text('Get Ready', 360, 96, 'rgb(0,0,255)');
+    }
+
+    if (winner)
+    {
+        game.debug.text('You Win!', 360, 32, 'rgb(0,0,255)');
+    }
+    else if (loser)
+    {
+        game.debug.text('You Lose!', 360, 32, 'rgb(0,0,255)');
+    }
+
+}
